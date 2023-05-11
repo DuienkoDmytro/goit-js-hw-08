4da5eaa50f1c501c673267e8998ad23f1002fe90
DuienkoDmytro <119892965+DuienkoDmytro@users.noreply.github.com>
2023-05-11 13:49:41 +0300

    question_3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,24 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector("iframe");
-
-
+const player = new Player(iframe);
+const localStorageTimePlayedKey = "videoplayer-current-time";
 
 const onPlay = function(data) {
     localStorage.setItem(localStorageTimePlayedKey, data.seconds);
-    console.log(data);
-};
-const localStorageTimePlayedKey = "videoplayer-current-time";
-player.on('timeupdate', onPlay);
+    console.log(data.seconds)
+  };
 
+player.on('timeupdate', throttle(onPlay, 1000));
 
-
-
-player.setCurrentTime(getItem.localStorageTimePlayedKey).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
+player.setCurrentTime(localStorage.getItem(localStorageTimePlayedKey) || 0).then(function(seconds) {
+    onPlay
 }).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
             break;
-
-        default:
-            // some other error occurred
+        default:        
             break;
     }
 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+// import throttle from "lodash.throttle";
 
+const inputForm = document.querySelector(`input`);
+const submit = document.querySelector(`submit`);
+const feedBackForm = document.querySelector(`feedback-form`);
+const enteredDate = {};
+const KEY = "feedback-form-state";
+
+feedBackForm.addEventListener(onInputForm, inputForm);
+feedBackForm.addEventListener(onBTN, submit);
+      
+function onInputForm(e) {
+    e.preventDefault();
+    if feedBackForm.value === "" { console.log(input required!) } 
+    
+    {console.log currentTarget.value}
+    
+}
